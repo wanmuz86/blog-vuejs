c88a84de1ed6533d1d70f9f178fa1e37aedbfa3e
Wan Muzaffar <wanmuz86@gmail.com>
2021-07-09 16:15:43 +0800

    add item

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,70 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 export default new Vuex.Store({
- state: {
-    },
- getters: {
-    
-},
- mutations: {
-   
- },
- actions: {
-  
- }
+   state: {
+      posts: [],
+      selectedPost:null
+   },
+   getters: {
+
+   },
+   mutations: {
+      setPosts(state, posts) {
+         state.posts = posts;
+      },
+      setPost(state,post){
+         state.selectedPost = post
+      }
+
+   },
+   actions: {
+      async getPosts({  commit }) {
+         try {
+            fetch('https://serene-crag-93594.herokuapp.com/api/posts')
+               .then(response => response.json())
+               .then(data => {
+                  commit('setPosts', data.data)
+            });
+         } catch (error) {
+            commit('setPosts', []);
+         }
+      },
+      async getPostById({  commit }, id) {
+         try {
+            fetch(`https://serene-crag-93594.herokuapp.com/api/posts/${id}`)
+               .then(response => response.json())
+               .then(data => {
+                  console.log(data)
+                  commit('setPost', data.data)
+            });
+         } catch (error) {
+            commit('setPost', null);
+         }
+      },
+      async updatePost({commit,state},id){
+         try {
+         
+            fetch(`https://serene-crag-93594.herokuapp.com/api/posts/${id}`, {
+               method: 'put',
+               headers: {
+                 "Content-type": "application/json"
+               },
+               body: JSON.stringify(state.selectedPost)
+             })
+             .then()
+             .then( (data)=> {
+              
+               commit('setPost',data.data)
+             })
+             .catch(function (error) {
+               console.log('not ok')
+               console.log('Request failed', error);
+             });
+         }
+         catch (error){
+            commit('setPost',null)
+         }
+      }
+   }
 })
+
--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,79 +1,57 @@
 <template>
-<v-container>
+  <v-container>
     <h1>Edit the page</h1>
-     <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
-    <v-textarea
-      v-model="description"
-      label="Description"
-      required
-    ></v-textarea>
+    <v-form ref="form" v-model="valid" lazy-validation v-if="selectedPost">
+      <v-text-field v-model="selectedPost.title" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+      <v-textarea v-model="selectedPost.description" label="Description" required></v-textarea>
 
-    <v-checkbox
-      v-model="checkbox"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
+      <v-checkbox v-model="checkbox" label="Do you agree?" required></v-checkbox>
 
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="validate"
-    >
-      Submit
-    </v-btn>
-
-  </v-form>
-</v-container>
-  
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Submit</v-btn>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 
+export default {
+  name: "Edit",
 
-  export default {
-    name: 'Edit',
+  created() {
+    this.getPostById(this.$route.params.id)
+  },
+  computed: {
+    ...mapState(["selectedPost"])
+  },
 
-     data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
-      description: '',
-   
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
-    }),
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      v => !!v || "Name is required",
+    ],
+    description: "",
 
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: false
+  }),
+
+  methods: {
+    ...mapActions(["getPostById", "updatePost"]),
+    validate() {
+      this.$refs.form.validate();
+      this.updatePost(this.$route.params.id).then(()=>{
+       this.$route.push('/')
+        })
     },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    }
   }
+};
 </script>
